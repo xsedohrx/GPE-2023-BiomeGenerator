5f7a9eca737c184e92e7960c0e72bbf8000d5b62
johnathan <johnathan.claasen@hva.nl>
2023-09-28 14:03:20 +0200

    1st attempt at marching cubes

--- Assets/Editor/BiomeDesigner.cs
+++ Assets/Editor/BiomeDesigner.cs
@@ -10,28 +10,30 @@ public class BiomeDesigner : EditorWindow
     BiomeScriptableObject biomeSO;
     private List<GameObject> biomes;
 
+    #region DrawGUI
     [MenuItem("Custom Menu/Open custom Window", false, 1)]
-    public static void OpenWindow() { 
+    public static void OpenWindow()
+    {
         BiomeDesigner window = GetWindow<BiomeDesigner>("Custom Window");
         window.Show();
     }
 
-    private void OnGUI(){ DrawBiomeEditorDesigner(); }
+    private void OnGUI() { DrawBiomeEditorDesigner(); }
 
     private void DrawBiomeEditorDesigner()
     {
 
         GUILayout.Box("Biome Designer", GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true));
         GUILayout.BeginArea(new Rect(10, 60, position.width - 20, position.height - 20));
-            DrawBiomeProperties();
-            DrawGenerateButtons();
-    
+        DrawBiomeProperties();
+        DrawGenerateButtons();
+
         GUILayout.EndArea();
     }
 
     private void DrawBiomeProperties()
     {
-//        GUILayout.Label(, GUILayout.Width(200), GUILayout.Height(20));
+        //        GUILayout.Label(, GUILayout.Width(200), GUILayout.Height(20));
         GUILayout.TextArea("Welcome to Biome Designer" + System.Environment.NewLine + "Insert a Biome Scriptable Object to start editing the biome properties");
         biomeSO = (BiomeScriptableObject)EditorGUILayout.ObjectField("Biome SO", biomeSO, typeof(BiomeScriptableObject), false);
         GUILayout.Label("X:", GUILayout.Width(200), GUILayout.Height(20));
@@ -62,5 +64,10 @@ public class BiomeDesigner : EditorWindow
             biomes.Clear();
         }
     }
+
+
+    #endregion
+
+
 }
 
